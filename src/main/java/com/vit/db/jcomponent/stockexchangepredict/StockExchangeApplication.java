35708a2cfe955f682874867d969260dd64f04595
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2021-04-14 19:43:23 +0530

    Change related to application

--- src/main/java/com/vit/db/jcomponent/stockexchangepredict/StockExchangeApplication.java
+++ src/main/java/com/vit/db/jcomponent/stockexchangepredict/StockExchangeApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class StockExchangeApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(StockExchangeApplication.class, args);
 	}
 
